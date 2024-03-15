93cc39a0b75547d49e83b9980df7e399830cab62
224lpalm@frhsd.com <224lpalm@frhsd.com>
2024-03-15 08:43:35 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+  
+    var magic8Ball = {};
+    magic8Ball.listofanswers = ["For sure", "Maybe", "No", "Yes","Most likely", "Looks good", "Yes", "Signs point to no", "Definetly", "Nah", "Would not say so", "Doubtful"];
+   
+    magic8Ball.getAnswer = function(question)
+    {
+      var randomNumber = Math.random();
+      var randomAnswer = Math.floor(randomNumber * this.listofanswers.length);
+      var answer = this.listofanswers[randomAnswer];
+      
+      $("#8ball").effect( "shake" );
+      $("#answer").text( answer );
+      $("#answer").fadeIn(3000);
+      $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");
+  
+      console.log(question);
+      console.log(answer);
+    };
+    $("#answer").hide();
+  
+    var onClick = function()
+    {
+      $("#answer").hide();
+      $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballQuestion.png");
+      var question = prompt("What do you want to know?");
+      magic8Ball.getAnswer(question);
+    };
+    
+    $("#questionButton").click( onClick );
+  });
